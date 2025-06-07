7ca2b9a9f72fac435896713a6152c1b68997e6a5
Prajwal N <prajja1990@gmail.com>
2025-06-07 14:49:45 +0530

    added security

--- src/main/java/com/inops/query/record/Organization.java
+++ src/main/java/com/inops/query/record/Organization.java
@@ -4,92 +4,117 @@ import java.util.List;
 import org.bson.types.ObjectId;
 
 public record Organization(
-    ObjectId _id,
-    String organizationName,
-    String organizationCode,
-    String addressLine1,
-    String addressLine2,
-    String city,
-    String pinCode,
-    String logoFileName,
-    String description,
-    String emailId,
-    String contactPersonContactNumber,
-    String registrationNo,
-    String tenantCode,
-    Integer isActive,
-    Integer firstMonthOfFinancialYear,
-    List<OrgSubsidiary> subsidiaries,
-    List<OrgDesignation> designations,
-    List<OrgGrade> grades,
-    List<OrgDivision> divisions,
-    List<OrgDepartment> departments,
-    List<OrgSubDepartment> subDepartments,
-    List<OrgSection> sections
+        ObjectId _id,
+        String organizationName,
+        String organizationCode,
+        String addressLine1,
+        String addressLine2,
+        String city,
+        String pinCode,
+        String logoFileName,
+        String description,
+        String emailId,
+        String contactPersonContactNumber,
+        String registrationNo,
+        String tenantCode,
+        Integer isActive,
+        Integer firstMonthOfFinancialYear,
+        List<OrgSubsidiary> subsidiaries,
+        List<OrgDesignation> designations,
+        List<OrgGrade> grades,
+        List<OrgDivision> divisions,
+        List<OrgDepartment> departments,
+        List<OrgSubDepartment> subDepartments,
+        List<OrgSection> sections,
+        List<OrgEmployeeCategory> employeeCategories,
+        List<OrgLocation> location
 ) {
 
     public record OrgSubsidiary(
-        String subsidiaryName,
-        String subsidiaryCode,
-        String subsidiaryDescription,
-        List<OrgSubLocation> location
-    ) {}
-
-    public record OrgSubLocation(
-        String locationName,
-        String locationCode
+            String subsidiaryName,
+            String subsidiaryCode,
+            String subsidiaryDescription,
+            String organizationCode,
+            String locationCode
     ) {}
 
     public record OrgLocation(
-        String locationName,
-        String locationCode,
-        String regionCode,
-        String countryCode,
-        String stateCode,
-        String city,
-        String pinCode
+            String locationName,
+            String locationCode,
+            String regionCode,
+            String countryCode,
+            String stateCode,
+            String city,
+            String pinCode,
+            String organizationCode
     ) {}
 
     public record OrgDesignation(
-        String divisionCode,
-        String designationCode,
-        String designationName,
-        String designationDescription
+            String divisionCode,
+            String subsidiaryCode,
+            String organizationCode,
+            String locationCode,
+            String designationCode,
+            String designationName,
+            String designationDescription
     ) {}
 
     public record OrgDivision(
-        String subsidiaryCode,
-        String divisionCode,
-        String divisionName,
-        String divisionDescription
+            String subsidiaryCode,
+            String organizationCode,
+            String locationCode,
+            String divisionCode,
+            String divisionName,
+            String divisionDescription
     ) {}
 
     public record OrgDepartment(
-        String departmentName,
-        String departmentCode,
-        String divisionCode,
-        String departmentDescription
+            String departmentName,
+            String departmentCode,
+            String divisionCode,
+            String subsidiaryCode,
+            String organizationCode,
+            String locationCode,
+            String departmentDescription
     ) {}
 
     public record OrgGrade(
-        String gradeCode,
-        String gradeName,
-        String gradeDescription,
-        String designationCode
+            String gradeCode,
+            String gradeName,
+            String gradeDescription,
+            String divisionCode,
+            String subsidiaryCode,
+            String organizationCode,
+            String locationCode,
+            String designationCode
     ) {}
 
     public record OrgSubDepartment(
-        String subDepartmentName,
-        String subDepartmentCode,
-        String departmentCode,
-        String subDepartmentDescription
+            String subDepartmentName,
+            String subDepartmentCode,
+            String departmentCode,
+            String divisionCode,
+            String subsidiaryCode,
+            String organizationCode,
+            String locationCode,
+            String subDepartmentDescription
     ) {}
 
     public record OrgSection(
-        String sectionName,
-        String sectionCode,
-        String subDepartmentCode,
-        String sectionDescription
+            String sectionName,
+            String sectionCode,
+            String subDepartmentCode,
+            String departmentCode,
+            String divisionCode,
+            String subsidiaryCode,
+            String organizationCode,
+            String locationCode,
+            String sectionDescription
+    ) {}
+    public record OrgEmployeeCategory(
+            String employeeCategoryCode,
+            String employeeCategoryName,
+            String employeeCategoryDescription
     ) {}
 
 }

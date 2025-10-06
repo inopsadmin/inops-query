d31ad863e0eec87b7eccbe01334b98655cbf95ae
yamini <yamini@inops.tech.com>
2025-10-06 12:31:03 +0530

    Adjusted Contractor WorkOrder GraphQL fields to align with UI

--- src/main/java/com/inops/query/record/Contractor.java
+++ src/main/java/com/inops/query/record/Contractor.java
@@ -3,66 +3,28 @@ package com.inops.query.record;
 import java.util.List;
 
 public record Contractor(
-	    String _id,
-	    String contractorCode,
-	    String contractorName,
-	    String organizationCode,
-	    String tenantCode,
+		String _id,
+		String contractorCode,
+		String contractorName,
+		String organizationCode,
+		String tenantCode,
 
-	    String ownerName,
-	    String ownerContactNo,
-	    String ownerEmailId,
-	    String fatherName,
-	    String contactPersonName,
-	    String contactPersonContactNo,
-	    String contactPersonEmailId,
-	    String birthDate,
-	    String workLocation,
-	    String aadharNumber,
-	    String contractorImage,
-	    String serviceSince,
-	    String typeOfCompany,
-	    Boolean penaltiesPolicies,
-	    Boolean basicInformation,
-	    Boolean companyDetails,
-	    Boolean licensesPermits,
-	    Boolean financialDetails,
-	    Boolean documentsCompliance,
-	    Boolean workOrdersCompleted,
-	    Boolean addressInformation,
-	    String panNumber,
-	    List<ContractorWorkOrder> workOrders,
+		String ownerName,
+		String ownerContactNo,
+		String ownerEmailId,
+		String fatherName,
+		String aadharNumber,
+		List<ContractorWorkOrder> workOrders,
 		Boolean isDeleted,
 		String createdOn,
 		String createdBy
-	) {
-    public record ContractorWorkOrder(
-        String workOrderNumber,
-        String workOrderDate,
-        String proposalReferenceNumber,
-        int NumberOfEmployee,
-        String contractPeriodFrom,
-        String contractPeriodTo,
-        String workOrderDocumentFilePath,
-        String annexureFilePath,
-        double serviceChargeAmount,
-        String workOrderType,
-        String workOrderLineItems,
-        String serviceLineItems,
-        String serviceCode,
-        double wcChargesPerEmployee,
-        List<AssetChargesPerDay> assetChargesPerDay,
-        EmployeeWages employeeWages
-    ) {}
-
-    public record EmployeeWages(
-        String wageType,
-        double wageAmount
-    ) {}
-
-	public record AssetChargesPerDay(
-			String assetCode,
-			String assetName,
-			Integer assetCharges
+) {
+	public record ContractorWorkOrder(
+			String workOrderNumber,
+			String workOrderDate,
+			String proposalReferenceNumber,
+			int NumberOfEmployee,
+			String contractPeriodFrom,
+			String contractPeriodTo
 	) {}
 }
